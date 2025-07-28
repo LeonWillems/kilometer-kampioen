a3944f216bcc6bbebfb81a7a59fb01c3b7a16a99
Leon Willems <leon.willems@bdo.nl>
2025-07-28 20:05:10 +0200

    - Make a nice image for the first set of mock stations and include their actual timetable - Include some more information from the official website and their affiliated developer - Acquire distances between stations and process for easier retrieval - Include official NS station codes - Update README accordingly

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# kilometer-kampioen
+# Kilometer Kampioen
 Drive as many kilometers by train as possible within 24 hours - with the help of a computer
+
+# Sources
+- Find all rules here: https://www.kilometerkampioen.nl/
+- Find all station-related content here: https://en.wikipedia.org/wiki/Railway_stations_in_the_Netherlands
+    - Each station has a code. For example, "Eindhoven Centraal" has "Ehv". Please find all codes in "stations/station_codes.csv"
+- Find kilometer count courtesy here: https://github.com/nanderv/trainkms
+    - A written guide how to count kilometers
+    - A tool for counting kilometers
+    - Number of kilometers between pairs of stations ("data/distance_between_stations.json")
+
+# V0
+## Data (see "data/mock_..." files)
+- Intercity stations: 's-Hertogenbosch, Tilburg, Eindhoven Centraal
+- Other stations: Vught, Boxtel, Best, Oisterwijk, Eindhoven Strijp-S
+- Timeframe: Saturday, 12:00 - 15:00
+- Actual train times for Saturday August 2nd, 2025
---
+++ data_processing/process_station_distances.py
@@ -0,0 +1,24 @@
+import json
+
+DISTANCES_FILE = "./data/station_distances.json"
+DISTANCES_PROCESSED_FILE = "./data/station_distances_processed.json"
+
+
+with open(DISTANCES_FILE, mode='r') as f:
+    # JSON contains a list of structured dictionaries
+    data = json.load(f)
+
+station_distances_processed = {}
+
+for station_distance in data:
+    from_station = station_distance['fromStation'].title()  # Capitalize first letter
+    to_station = station_distance['toStation'].title()  # Capitalize first letter
+    distance = station_distance['distance'] / 10  # Convert from hectometers to kilometers
+
+    # Check if key (from_station) already exists, if not, initialize empty dictionary
+    # Then, for from_station dict, set key (to_station) with value distance
+    # We use dictionaries for quick, easy and intuitive lookups (dict[from][to] yields dist)
+    station_distances_processed.setdefault(from_station, {})[to_station] = distance
+
+with open(DISTANCES_PROCESSED_FILE, mode='w') as f:
+    json.dump(station_distances_processed, f)
