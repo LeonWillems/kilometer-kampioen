8467e5cc9a971b607103095119d23edd58d899be
LeonWillems <leon_willems@msn.com>
2026-01-13 21:37:37 +0100

    Adjusted `speed_profiling.py` for new structure

--- tests/speed_profiling.py
+++ tests/speed_profiling.py
@@ -1,34 +1,23 @@
-import json
 import pstats
 import cProfile
-
 from datetime import datetime
 
-from ..settings import Settings
 from ..route_finding.greedy_dfs import run_greedy_dfs
 
+"""Speed profiling.
+Caveat: runs the script in a non-interruptible way, so Ctrl+C won't work!
+Use the following command liner instead:
+`python -m cProfile -o speed.prof -m kilometer-kampioen.run`
+
+Run the following in any case to inspect the results:
+`snakeviz ./speed.prof`
+"""
 
 if __name__ == "__main__":
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
 
-    parameters = {
-        'version': 'v0',
-        'start_station': 'Ht',
-        'start_time': '12:00',
-        'end_time': '15:00',
-        'min_transfer_time': 3,
-        'max_transfer_time': 15,
-        'timestamp': timestamp,  # Automatically generated
-    }
-
-    parameters_path = Settings.VERSIONED_PARAMETERS_PATH[parameters['version']]
-    json_file_path = (parameters_path / timestamp).with_suffix('.json')
-
-    with open(json_file_path, 'w') as f:
-        json.dump(parameters, f)
-
     with cProfile.Profile() as pr:
-        run_greedy_dfs(parameters)
+        run_greedy_dfs(timestamp)
 
         stats = pstats.Stats(pr)
         stats.sort_stats(pstats.SortKey.TIME)
